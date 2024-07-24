0457ecd1a1c5ac885dffcae997cc8cf7d035f0d6
aashiq.a <aashiq.ahamed@scicom.com.my>
2024-07-24 12:29:28 +0530

    Change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,23 @@
 import { RouterOutlet } from '@angular/router';
 import { MatDialog } from "@angular/material/dialog";
 import { MatButtonModule } from "@angular/material/button";
-import { MatAutocompleteModule, MatOption } from '@angular/material/autocomplete';
+import {MatAutocompleteModule, MatAutocompleteSelectedEvent, MatOption} from '@angular/material/autocomplete';
 import { MatInputModule } from '@angular/material/input';
 import { ReactiveFormsModule } from '@angular/forms';
-import { AsyncPipe, NgFor } from '@angular/common';
+import {AsyncPipe, NgFor, NgIf} from '@angular/common';
 
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { map, startWith } from 'rxjs/operators';
-
+export interface Employee {
+  id: string;
+  name: string;
+}
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatButtonModule, MatAutocompleteModule, MatInputModule, ReactiveFormsModule, NgFor, MatOption, AsyncPipe],
+  imports: [RouterOutlet, MatButtonModule, MatAutocompleteModule, MatInputModule, ReactiveFormsModule, NgFor, MatOption, AsyncPipe,NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 
@@ -25,11 +28,16 @@ export class AppComponent implements OnInit {
   constructor(public dialog: MatDialog) { }
 
   employeeForm: FormGroup;
-  employeeIds: string[] = ['E001', 'E002', 'E003', 'E004'];
-  employeeNames: string[] = ['John Doe', 'Jane Smith', 'Alice Johnson', 'Bob Brown'];
+  employees: Employee[] = [
+    { id: 'E001', name: 'John Doe' },
+    { id: 'E002', name: 'Jane Smith' },
+    { id: 'E003', name: 'Alice Johnson' },
+    { id: 'E004', name: 'Bob Brown' }
+  ];
 
-  filteredEmployeeIds: Observable<string[]>;
-  filteredEmployeeNames: Observable<string[]>;
+  filteredEmployeeIds: Observable<Employee[]>;
+  filteredEmployeeNames: Observable<Employee[]>;
+  selectedEmployee:Employee;
 
   ngOnInit() {
     this.employeeForm = new FormGroup({
@@ -39,22 +47,29 @@ export class AppComponent implements OnInit {
 
     this.filteredEmployeeIds = this.employeeForm.get('employeeId')!.valueChanges.pipe(
       startWith(''),
-      map(value => this._filterIds(value))
+      map(value => this._filter(value, 'id'))
     );
 
     this.filteredEmployeeNames = this.employeeForm.get('employeeName')!.valueChanges.pipe(
       startWith(''),
-      map(value => this._filterNames(value))
+      map(value => this._filter(value, 'name'))
     );
+
   }
 
-  private _filterIds(value: any): string[] {
+  private _filter(value: string, field: 'id' | 'name'): Employee[] {
     const filterValue = value.toLowerCase();
-    return this.employeeIds.filter(id => id.toLowerCase().includes(filterValue));
+    return this.employees.filter(employee => employee[field].toLowerCase().includes(filterValue));
   }
 
-  private _filterNames(value: any): string[] {
-    const filterValue = value.toLowerCase();
-    return this.employeeNames.filter(name => name.toLowerCase().includes(filterValue));
+  onEmployeeIdSelected($event: MatAutocompleteSelectedEvent) {
+    console.log($event);
+    const selectedId = $event.option?.value;
+    this.selectedEmployee = this.employees.filter(emp=> emp.id == selectedId)[0];
+  }
+
+  onEmployeeNameSelected($event: MatAutocompleteSelectedEvent) {
+    const selectedName = $event.option?.value;
+    this.selectedEmployee = this.employees.filter(emp=> emp.name == selectedName)[0];
   }
 }
