4d56a34472d82ddb5c27e75afd1c01e22b01a147
aashiq.a <aashiq.ahamed@scicom.com.my>
2024-07-24 12:47:29 +0530

    changechange

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,16 @@
 import { RouterOutlet } from '@angular/router';
 import { MatDialog } from "@angular/material/dialog";
 import { MatButtonModule } from "@angular/material/button";
-import {MatAutocompleteModule, MatAutocompleteSelectedEvent, MatOption} from '@angular/material/autocomplete';
+import { MatAutocompleteModule, MatAutocompleteSelectedEvent, MatOption } from '@angular/material/autocomplete';
 import { MatInputModule } from '@angular/material/input';
 import { ReactiveFormsModule } from '@angular/forms';
-import {AsyncPipe, NgFor, NgIf} from '@angular/common';
+import { AsyncPipe, NgFor, NgIf } from '@angular/common';
 
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { map, startWith } from 'rxjs/operators';
+import { MatCard, MatCardContent, MatCardHeader, MatCardTitle } from "@angular/material/card";
 export interface Employee {
   id: string;
   name: string;
@@ -17,7 +18,8 @@ export interface Employee {
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatButtonModule, MatAutocompleteModule, MatInputModule, ReactiveFormsModule, NgFor, MatOption, AsyncPipe,NgIf],
+  imports: [RouterOutlet, MatButtonModule, MatAutocompleteModule, MatInputModule,
+    ReactiveFormsModule, NgFor, MatOption, AsyncPipe, NgIf, MatCard, MatCardHeader, MatCardContent, MatCardTitle],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 
@@ -37,7 +39,7 @@ export class AppComponent implements OnInit {
 
   filteredEmployeeIds: Observable<Employee[]>;
   filteredEmployeeNames: Observable<Employee[]>;
-  selectedEmployee:Employee;
+  selectedEmployee: Employee;
 
   ngOnInit() {
     this.employeeForm = new FormGroup({
@@ -65,13 +67,13 @@ export class AppComponent implements OnInit {
   onEmployeeIdSelected($event: MatAutocompleteSelectedEvent) {
     console.log($event);
     const selectedId = $event.option?.value;
-    this.selectedEmployee = this.employees.filter(emp=> emp.id == selectedId)[0];
+    this.selectedEmployee = this.employees.filter(emp => emp.id == selectedId)[0];
     this.employeeForm.get('employeeName')?.setValue(this.selectedEmployee.name);
   }
 
   onEmployeeNameSelected($event: MatAutocompleteSelectedEvent) {
     const selectedName = $event.option?.value;
-    this.selectedEmployee = this.employees.filter(emp=> emp.name == selectedName)[0];
+    this.selectedEmployee = this.employees.filter(emp => emp.name == selectedName)[0];
     this.employeeForm.get('employeeId')?.setValue(this.selectedEmployee.id);
   }
 }
