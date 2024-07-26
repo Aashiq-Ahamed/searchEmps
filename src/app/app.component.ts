6a10bcf5462ad7c4dc84e70cbabcbb16794bffe7
aashiq.a <aashiq.ahamed@scicom.com.my>
2024-07-27 01:21:37 +0530

    Change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,17 @@ import { FormControl, FormGroup } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { map, startWith } from 'rxjs/operators';
 import { MatCard, MatCardContent, MatCardHeader, MatCardTitle } from "@angular/material/card";
-export interface Employee {
+import {EmployeeTableComponent} from "./employee-table/employee-table.component";
+import {
+  MatCell, MatCellDef,
+  MatColumnDef,
+  MatHeaderCell, MatHeaderCellDef,
+  MatHeaderRow, MatHeaderRowDef, MatRow, MatRowDef,
+  MatTable,
+  MatTableDataSource
+} from "@angular/material/table";
+import {EmployeeService} from "./service/employee.service";
+interface Employee {
   id: string;
   name: string;
 }
@@ -19,61 +29,41 @@ export interface Employee {
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, MatButtonModule, MatAutocompleteModule, MatInputModule,
-    ReactiveFormsModule, NgFor, MatOption, AsyncPipe, NgIf, MatCard, MatCardHeader, MatCardContent, MatCardTitle],
+    ReactiveFormsModule, NgFor, MatOption, AsyncPipe, NgIf, MatCard, MatCardHeader, MatCardContent, MatCardTitle, MatTable, MatColumnDef, MatHeaderCell, MatCell, MatHeaderRow, MatRow, MatHeaderCellDef, MatCellDef, MatHeaderRowDef, MatRowDef],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 
 })
 export class AppComponent implements OnInit {
-  title = 'searchEmps';
+  displayedColumns: string[] = ['id', 'name','typeA','typeB', 'actions'];
 
-  constructor(public dialog: MatDialog) { }
 
-  employeeForm: FormGroup;
-  employees: Employee[] = [
-    { id: 'E001', name: 'John Doe' },
-    { id: 'E002', name: 'Jane Smith' },
-    { id: 'E003', name: 'Alice Johnson' },
-    { id: 'E004', name: 'Bob Brown' }
-  ];
+  constructor(public dialog: MatDialog,
+              public empService :EmployeeService) { }
 
-  filteredEmployeeIds: Observable<Employee[]>;
-  filteredEmployeeNames: Observable<Employee[]>;
-  selectedEmployee: Employee;
+  // employees: Employee[] = [
+  //   { id: 'E001', name: 'John Doe' },
+  //   { id: 'E002', name: 'Jane Smith' },
+  //   { id: 'E003', name: 'Alice Johnson' },
+  //   { id: 'E004', name: 'Bob Brown' }
+  // ];
+
 
   ngOnInit() {
-    this.employeeForm = new FormGroup({
-      employeeId: new FormControl(''),
-      employeeName: new FormControl('')
+    this.empService.employees.data.push({ id: 'E001', name: 'John Doe', typeA: 'test1', typeB:'test1'},
+      { id: 'E002', name: 'Jane Smith', typeA: 'test1', typeB:'test1' },
+      { id: 'E003', name: 'Alice Johnson',typeA: 'test1', typeB:'test1' },
+      { id: 'E004', name: 'Bob Brown', typeA: 'test1', typeB:'test1'})
+  }
+
+  openEditPopup(index: number): void {
+    const dialogRef = this.dialog.open(EmployeeTableComponent, {
+      width: '250px',
+      data: index
     });
 
-    this.filteredEmployeeIds = this.employeeForm.get('employeeId')!.valueChanges.pipe(
-      startWith(''),
-      map(value => this._filter(value, 'id'))
-    );
-
-    this.filteredEmployeeNames = this.employeeForm.get('employeeName')!.valueChanges.pipe(
-      startWith(''),
-      map(value => this._filter(value, 'name'))
-    );
-
-  }
-
-  private _filter(value: string, field: 'id' | 'name'): Employee[] {
-    const filterValue = value.toLowerCase();
-    return this.employees.filter(employee => employee[field].toLowerCase().includes(filterValue));
-  }
-
-  onEmployeeIdSelected($event: MatAutocompleteSelectedEvent) {
-    console.log($event);
-    const selectedId = $event.option?.value;
-    this.selectedEmployee = this.employees.filter(emp => emp.id == selectedId)[0];
-    this.employeeForm.get('employeeName')?.setValue(this.selectedEmployee.name);
-  }
-
-  onEmployeeNameSelected($event: MatAutocompleteSelectedEvent) {
-    const selectedName = $event.option?.value;
-    this.selectedEmployee = this.employees.filter(emp => emp.name == selectedName)[0];
-    this.employeeForm.get('employeeId')?.setValue(this.selectedEmployee.id);
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
   }
 }
